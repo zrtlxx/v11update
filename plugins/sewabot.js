c8a8d81f92036407b2ddda88dd30d44fd4b11561
zrtlxx <110093701+zrtlxx@users.noreply.github.com>
2022-08-05 04:17:39 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -13,16 +13,16 @@ let handler = async (m, { conn }) => {
   description: ` *• SEWA BOT & UP TO PREMIUM •*
         
 1. Grup / 30 Hari
-Rp. 15.000 Dana
-Rp. 15.000 Pulsa
+Rp. 15.000 Gopay
+Rp. 20.000 Pulsa
 
 2. Premium / 30 Hari
-Rp. 10.000 Dana
-Rp. 10.000 Pulsa
+Rp. 10.000 Gopay 
+Rp. 15.000 Pulsa
 
 3. Premium + Grup / 30 Hari
-Rp. 20.000 Dana
-Rp. 20.000 Pulsa
+Rp. 20.000 Gopay
+Rp. 25.000 Pulsa
 
 wa.me/${owner[0]}
 *Bukan Bot!!!*
